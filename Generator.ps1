10ccdeacc437e679be132a19f32e52f11370314f
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2024-09-27 10:43:40 +0100

    0.0.3

--- Generator.ps1
+++ Generator.ps1
@@ -1,4 +1,4 @@
-$ClientName = (Read-Host -Prompt "Please Enter Client Name").ToUpper()
+$ClientName = (Read-Host -Prompt "Please Enter Client Name").ToUpper() -replace ' ', "_"
 
 $EnvFilePath = Join-Path $PWD "\.env"
 Write-Host $EnvFilePath
@@ -12,7 +12,6 @@ else
 {
     Write-Host "Unable to locate .env in root"
 }
-    
     
 $IV = openssl.exe rand -base64 16 
 
@@ -36,15 +35,20 @@ $cryptoStream.FlushFinalBlock()
 $encryptedBytes = $memoryStream.ToArray()
 
 $connection = New-Object System.Data.SqlClient.SqlConnection
-$connection.ConnectionString = $conn 
-$connection.OpenAsync() | Out-Null
+$connection.ConnectionString = $connString 
+$connectionAsync = $connection.OpenAsync()
+$connectionAsync.GetAwaiter().GetResult()
 
 $command = $connection.CreateCommand()
 $command.CommandText = @"
-INSERT INTO [dbo].[Clients] ([name], [secret], [iv])  VALUES ([$ClientName],[$encryptedBytes],[$encryptedIV])
+INSERT INTO [dbo].[Clients] ([name], [secret], [iv])  VALUES (@Value1, @Value2, @Value3)
 "@
 
+$command.Parameters.Add("@Value1", [System.Data.SqlDbType]::VarChar).Value = $ClientName
+$command.Parameters.Add("@Value2", [System.Data.SqlDbType]::VarBinary).Value = $encryptedBytes
+$command.Parameters.Add("@Value3", [System.Data.SqlDbType]::VarBinary).Value = $encryptedIV
+
 $executeTask = $command.ExecuteNonQueryAsync()
-$executeTask.Wait()
+$executeTask.GetAwaiter().GetResult()
 
 

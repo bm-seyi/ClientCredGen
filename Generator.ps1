9d67f895582bde101f3499df0cb12e1327954110
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2024-10-16 20:44:36 +0100

    0.0.4

--- Generator.ps1
+++ Generator.ps1
@@ -1,7 +1,14 @@
-$ClientName = (Read-Host -Prompt "Please Enter Client Name").ToUpper() -replace ' ', "_"
+$ClientName = (Read-Host -Prompt "Please Enter Client Name").ToUpper()
+
+if (-not ($ClientName -match "^[a-zA-Z ]+$"))
+{
+    Write-Host "Input can only contain letters and spaces. Please try again."
+    exit
+}
+
+$ClientName = $ClientName -replace " ", "_"
 
 $EnvFilePath = Join-Path $PWD "\.env"
-Write-Host $EnvFilePath
 
 if (Test-Path $EnvFilePath -PathType Leaf) 
 {
@@ -11,27 +18,35 @@ if (Test-Path $EnvFilePath -PathType Leaf)
 else 
 {
     Write-Host "Unable to locate .env in root"
+    exit
 }
     
 $IV = openssl.exe rand -base64 16 
+$clientSecret = openssl.exe rand -hex 32 | Out-String
+$clientID = [guid]::NewGuid()
 
-$clientSecret = (openssl.exe rand -base64 32 | Out-String).TrimEnd('=') -replace '\+', '-' -replace '/', '_' 
+$downloadFile = [System.Environment]::GetFolderPath("UserProfile") + "\Downloads\clientsecret.txt"
+Set-Content -Path $downloadFile -Value "Client Name: $ClientName`nID: $clientID`nClient Secret: $clientSecret"
 
 $aes = [System.Security.Cryptography.Aes]::Create()
 
 $aes.KeySize = 256
+$aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
 
 $encryptedIV = [System.Convert]::FromBase64String($IV)
 
 $encryptor = $aes.CreateEncryptor([System.Convert]::FromHexString($encryptionKey), $encryptedIV)
 
+# Encrypt the plaintext
 $memoryStream = New-Object System.IO.MemoryStream
+$cryptoStream = New-Object System.Security.Cryptography.CryptoStream($memoryStream, $encryptor, [System.Security.Cryptography.CryptoStreamMode]::Write)
+$streamWriter = New-Object System.IO.StreamWriter($cryptoStream)
 
-$cryptoStream = New-Object System.Security.Cryptography.CryptoStream ($memoryStream, $encryptor, [System.Security.Cryptography.CryptoStreamMode]::Write)
-$streamWriter = New-Object System.IO.StreamWriter $cryptoStream
+# Write plaintext to the stream
 $streamWriter.Write($clientSecret)
-$streamWriter.Flush()
-$cryptoStream.FlushFinalBlock()
+$streamWriter.Close()  # This also closes cryptoStream and memoryStream
+
+# Get encrypted data
 $encryptedBytes = $memoryStream.ToArray()
 
 $connection = New-Object System.Data.SqlClient.SqlConnection
@@ -41,12 +56,13 @@ $connectionAsync.GetAwaiter().GetResult()
 
 $command = $connection.CreateCommand()
 $command.CommandText = @"
-INSERT INTO [dbo].[Clients] ([name], [secret], [iv])  VALUES (@Value1, @Value2, @Value3)
+INSERT INTO [dbo].[Clients] ([ID], [name], [secret], [iv])  VALUES (@Value1, @Value2, @Value3, @Value4)
 "@
 
-$command.Parameters.Add("@Value1", [System.Data.SqlDbType]::VarChar).Value = $ClientName
-$command.Parameters.Add("@Value2", [System.Data.SqlDbType]::VarBinary).Value = $encryptedBytes
-$command.Parameters.Add("@Value3", [System.Data.SqlDbType]::VarBinary).Value = $encryptedIV
+$command.Parameters.Add("@Value1", [System.Data.SqlDbType]::UniqueIdentifier).Value = [guid]::Parse($clientID)
+$command.Parameters.Add("@Value2", [System.Data.SqlDbType]::VarChar).Value = $ClientName
+$command.Parameters.Add("@Value3", [System.Data.SqlDbType]::VarBinary).Value = $encryptedBytes
+$command.Parameters.Add("@Value4", [System.Data.SqlDbType]::VarBinary).Value = $encryptedIV
 
 $executeTask = $command.ExecuteNonQueryAsync()
 $executeTask.GetAwaiter().GetResult()
